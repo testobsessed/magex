628e1c3b9f628b749cddeef2f3c66b7b961c96e9
Elisabeth Hendrickson <esh@qualitytree.com>
2013-02-18 10:27:55 -0800

    Adding comments to explain samples

--- client_sample_code/create_and_retrieve_account.rb
+++ client_sample_code/create_and_retrieve_account.rb
@@ -1,6 +1,19 @@
 $LOAD_PATH << "client_sample_code"
 require 'sample_helper'
 
-account = magex_post("/account/register", { :username => "unicorn" }.to_json)
+# Most of the API calls use JSON. To register an account you
+# need JSON with a "username" key. This script uses the ruby
+# JSON to_json method to turn a hash into a json string
+register_data_as_json = { :username => "unicorn" }.to_json
 
-magex_get("/account/status/#{account['secret']}")
+# This script uses a helper method from the sample_helper.rb file
+# See the helper file for implementation details.
+account = magex_post("/account/register", register_data_as_json)
+
+# You need the secret in order to query for account status.
+# If there was an error in creating the account there will be no secret available.
+# The most likely cause of the account not being created with this sample script
+# is violating the username uniqueness constraint.
+if account['secret']
+  magex_get("/account/status/#{account['secret']}")
+end
--- client_sample_code/sample_helper.rb
+++ client_sample_code/sample_helper.rb
@@ -1,6 +1,13 @@
 require 'rest_client'
 require 'json'
 
+# This helper file contains methods to manage the POST and GET requests
+# to the Magex server. It uses the rest_client library to make the HTTP
+# requests and adds messages that appear on stdout so you can tell
+# what is going on. It is intended to provide a reference for how you
+# might use the Magex API, but you'll probably want to modify it to
+# be less noisy when writing your own client.
+
 @base_uri = "http://localhost:9292"
 
 def magex_post(route, payload)
@@ -18,13 +25,23 @@ end
 def get_request(route)
   puts "======"
   puts "Getting #{@base_uri}#{route}"
-  RestClient.get("#{@base_uri}#{route}")
+  begin
+    response = RestClient.get("#{@base_uri}#{route}")
+  rescue => e
+    response = e.response
+  end
+  response
 end
 
 def post_request(route, payload)
   puts "======"
   puts "Posting to #{@base_uri}#{route} with data #{payload}"
-  RestClient.post("#{@base_uri}#{route}", payload)
+  begin
+    response = RestClient.post("#{@base_uri}#{route}", payload)
+  rescue => e
+    response = e.response
+  end
+  response
 end
 
 def display_result(response)
